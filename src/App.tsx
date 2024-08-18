e892639959fdf21d12481f864ce92abfba894cf6
nwy98 <97599088+norunaru@users.noreply.github.com>
2024-08-18 21:50:23 +0900

    init: 프로젝트 생성 및 라이브러리 설치

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div className="text-center">
+      <h1 className="text-4xl font-bold text-blue-600">Hello, Tailwind CSS!</h1>
+    </div>
+  );
+}
+
+export default App;

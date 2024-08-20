2447b73e19f72a2435f16317eea4900bc56be85c
Chaerin Jeong <74577662+chaerin-77@users.noreply.github.com>
2024-08-20 12:01:59 +0900

    [FIX] 글꼴 추가, 기본 너비 설정
    Pretendard 글꼴 추가 및 360px로 너비 설정

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 function App() {
   return (
-    <div className="text-center">
+    <div className="text-center w-[360px]">
       <h1 className="text-4xl font-bold text-blue-600">Hello, Tailwind CSS!</h1>
     </div>
   );

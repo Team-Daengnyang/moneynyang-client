704af284502ca146dec84175bfc987b9fd8cfba8
Chaerin Jeong <74577662+chaerin-77@users.noreply.github.com>
2024-08-20 14:12:44 +0900

    [CHORE] 깃 충돌 확인

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 function App() {
   return (
-    <div className="text-center w-[360px]">
+    // 너비 360px로 고정, status bar 크기 24px (mt-6)으로 고정
+    <div className="text-center w-[360px] mt-6">
       <h1 className="text-4xl font-bold text-main-color">
         Hello, Tailwind CSS!
       </h1>
---
+++ src/pages/Login/login.tsx
@@ -0,0 +1,7 @@
+export const Login = () => {
+  return (
+    <div className="w-full mx-auto bg-main-color px-4 text-center">
+      <img src="../../assets/Login/loginLogo.png" alt="" />
+    </div>
+  );
+};

398aeccabd0298bb30c27b66a1acc30f34e52872
노우영 <nwy98@naver.com>
2024-08-20 13:56:29 +0900

    [FIX] reset.css적용

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,16 @@
+import { Route, Routes } from "react-router-dom";
+
 function App() {
   return (
-    <div className="text-center w-[360px]">
-      <h1 className="text-4xl font-bold text-main-color">
-        Hello, Tailwind CSS!
-      </h1>
-    </div>
+    <>
+      <div className="text-center w-[360px]">
+        <h1 className="text-4xl font-bold text-main-color">
+          Helloooo, Tailwind CSS!
+        </h1>
+        <h3>asdfasdf</h3>
+      </div>
+      <div>삐끼삐끼</div>
+    </>
   );
 }
 
--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,14 @@
-import { StrictMode } from "react";
+// import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
 import "./fonts/Font.css";
+import { BrowserRouter } from "react-router-dom";
 
 createRoot(document.getElementById("root")!).render(
-  <StrictMode>
+  <BrowserRouter>
+    {/* <StrictMode> */}
     <App />
-  </StrictMode>
+    {/* </StrictMode> */}
+  </BrowserRouter>
 );

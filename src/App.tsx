a0499c6e88b9848b00ee6c288ff5208c545fe8f4
Chaerin Jeong <74577662+chaerin-77@users.noreply.github.com>
2024-08-20 12:07:35 +0900

    [FIX] 메인 테마 색상 지정
    메인 테마 색상 #1371FF로 지정

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,9 @@
 function App() {
   return (
     <div className="text-center w-[360px]">
-      <h1 className="text-4xl font-bold text-blue-600">Hello, Tailwind CSS!</h1>
+      <h1 className="text-4xl font-bold text-main-color">
+        Hello, Tailwind CSS!
+      </h1>
     </div>
   );
 }

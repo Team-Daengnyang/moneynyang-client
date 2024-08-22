815ac554633d7bc7f6f060b54bd909e61bab8aaf
Chaerin Jeong <74577662+chaerin-77@users.noreply.github.com>
2024-08-22 12:24:34 +0900

    [FEAT] 이미지 파일 경로 수정
    이미지 파일을 절대 경로로 수정

--- src/components/Topbar/index.tsx
+++ src/components/Topbar/index.tsx
@@ -8,14 +8,13 @@ interface TopBarProps {
 export const TopBar = ({ title, skip }: TopBarProps) => {
   const navigate = useNavigate();
   const place = skip ? "justify-between" : "space-x-16";
+  const leftAngle = `${
+    import.meta.env.VITE_PUBLIC_URL
+  }/src/assets/leftAngle.png`;
 
   return (
     <div className={`flex py-3 mb-3 ${place}`}>
-      <img
-        src="src/assets/leftAngle.png"
-        className="w-6"
-        onClick={() => navigate(-1)}
-      />
+      <img src={leftAngle} className="w-6" onClick={() => navigate(-1)} />
       <p className="text-lg font-semibold">{title}</p>
       {skip ? (
         <Link to={`${skip}`} className="text-main-color mt-1">
